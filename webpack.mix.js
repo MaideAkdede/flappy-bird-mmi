50adee486a9353b19b82e088b51d4b59d760c447
MaideAkdede <maide.akdede@student.hepl.com>
2021-03-17 15:52:32 +0100

    :tada: commit changes before starting

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,5 @@
 let mix = require('laravel-mix')
 
-mix.setPublicPath('dist/')
-    .copy('src/index.html', 'dist/index.html')
-    .copyDirectory('src/resources', 'dist/resources')
-    .js('src/js/main.js', 'dist/js/')
+mix.js('src/js/main.js', 'dist/js/')
     .sass('src/scss/main.scss', 'dist/css/')
+    .copyDirectory('src/resources', 'dist/resources');
