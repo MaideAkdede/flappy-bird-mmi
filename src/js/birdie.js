0ae75ced170af4c6bb78d94e3b0e3df799706ef9
MaideAkdede <maide.akdede@student.hepl.com>
2021-03-17 18:04:28 +0100

    :heavy_plus_sign: Add birdie + flap wings

---
+++ src/js/birdie.js
@@ -0,0 +1,47 @@
+import ground from "./ground";
+
+const birdie = {
+    game: null,
+    frames: [
+        {sx: 6, sy: 982},
+        {sx: 62, sy: 982},
+        {sx: 118, sy: 982}
+    ],
+    animationStep: 0,
+    maxAnimationStep: 2,
+    width: 34,
+    height: 24,
+    x: 0,
+    y: 0,
+    fallSpeed: 0,
+    maxFallSpeed: 7,
+    maxOffset: 0,
+    update() {
+        this.render();
+    },
+    init(game) {
+        this.game = game;
+        this.x = this.width / 2 + 3;
+        this.y = (game.canvas.height - ground.frame.sh) / 2;
+    },
+    render() {
+        this.animationStep = this.animationStep < this.maxAnimationStep ? this.animationStep + 1 : 0;
+        this.game.context.save();
+        this.game.context.translate(this.x, this.y);
+        this.game.context.rotate(0);
+        this.game.renderSpriteFrame(
+            {
+                sx: this.frames[this.animationStep].sx,
+                sy: this.frames[this.animationStep].sy,
+                sw: this.width,
+                sh: this.height,
+                dx: -this.width / 2,
+                dy: -this.height / 2,
+                dw: this.width,
+                dh: this.height,
+            }
+        )
+        this.game.context.restore();
+    }
+}
+export default birdie;
